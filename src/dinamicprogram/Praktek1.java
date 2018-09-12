e788df36d1c0543208d019be91f45b0fc93beafd
galih <galih@DESKTOP-DMHAKJQ.mshome.net>
2018-09-12 12:50:40 +0700

    initial comit

---
+++ src/dinamicprogram/Praktek1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dinamicprogram;
+
+/**
+ *
+ * @author galih
+ */
+public class Praktek1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void tampiljudul(String identitas) {
+        System.out.println("Identitas " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+    public static void main(String[] args) {
+        String identitas = "nama lengkap / kelas / no";
+        
+        tampiljudul(identitas);
+    }
+       
+    
+}
