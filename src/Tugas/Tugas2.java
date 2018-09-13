9a2abd8a6cf2205b2f2b7c53bfcdee9297948ca5
galih <galih@DESKTOP-DMHAKJQ.mshome.net>
2018-09-13 21:08:51 +0700

    jumlah

--- src/Tugas/Tugas2.java
+++ src/Tugas/Tugas2.java
@@ -5,14 +5,35 @@
  */
 package Tugas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author galih
  */
 public class Tugas2 {
     private static void tampiljudul (){
-        System.out.println("Hitung bayaran supir");
-        System.out.println("\nA = 4kg/5000; B = 5kg/7000; C = 2kg/2000; D = 3kg/3000; E =10000");
+        System.out.println("Hitung upah supir");
+        System.out.println("\nA = 4kg/5000; \nB = 5kg/7000; \nC = 2kg/2000; \nD = 3kg/3000; \nE = 7kg/10000");
     }
+ 
+    public static void main(String[] args) {
+    tampiljudul();
     
+    Scanner input = new Scanner(System.in);
+    System.out.println("Target Maximal Upah = ");
+    int uang = input.nextInt();
+    String nama [] = {"E","B","A","D","C"};
+    int berat [] = {7,5,4,3,2};
+    int harga [] = {10000,7000,5000,3000,2000};
+    int jumlah;
+    
+    for (int i = 0; i < harga.length; i++){
+        if (uang >= harga [i]){
+             jumlah = uang/harga[i];
+                uang = uang%harga[i];
+                System.out.println(" Barang " + nama[i] + " sebanyak " + jumlah + " buah ");
+    }
+    }
+    }
 }
