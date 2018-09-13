8bfe09b3ac1b89d18ccde41d44dad28e2b6ec88a
galih <galih@DESKTOP-DMHAKJQ.mshome.net>
2018-09-13 09:12:53 +0700

    tampil

---
+++ src/Tugas/Tugas2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author galih
+ */
+public class Tugas2 {
+    private static void tampiljudul (){
+        System.out.println("Hitung bayaran supir");
+        System.out.println("\nA = 4kg/5000; B = 5kg/7000; C = 2kg/2000; D = 3kg/3000; E =10000");
+    }
+    
+}
